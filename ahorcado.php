0e6f6c25258ede9577b556ab4e31d6c82de0de67
dblancoformacion <dblancoformacion@gmail.com>
2019-12-13 18:24:34 +0100

    Intentos

--- ahorcado.php
+++ ahorcado.php
@@ -8,7 +8,13 @@ else{
 	$p=$palabras[rand(1,count($palabras))-1];
 	$p=filtra_palabra($p);
 }
-
+if(isset($_POST['letra'])){
+	$l=strtoupper($_POST['letra']);
+	for($i=0;$i<iconv_strlen($p);$i++){
+		if($l==$p[$i])
+			$_POST['letras'][$i]=$l;
+	}
+}
 function formulario_resolver($p){
 	$r=null;
 	$r.='<form method="post">';
@@ -23,6 +29,19 @@ function formulario_resolver($p){
 	}
 	$r.='<input type="hidden" name="resultado" value="'.$p.'">';
 	$r.='<button>Resolver</button>';
+	$r.='</form>';
+	return $r;
+}
+function formulario_probar($p){
+	$r=null;
+	$r.='<form method="post">';
+	$r.='<input name="letra" size="1">';
+	if(isset($_POST['letras'])) for($i=0;$i<iconv_strlen($p);$i++){
+		if(!isset($_POST['letras'][$i])) $_POST['letras'][$i]='';
+		$r.='<input type="hidden" name="letras[]" value="'.$_POST['letras'][$i].'">';
+	}
+	$r.='<input type="hidden" name="resultado" value="'.$p.'">';
+	$r.='<button>Probar</button>';
 	$r.='</form>';
 	return $r;
 }
@@ -45,3 +64,4 @@ function filtra_palabra($p){
 	return $p;
 }
 echo formulario_resolver($p);
+echo formulario_probar($p);
