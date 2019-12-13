7b17d5560db5d5d0de2860ba1dabf5d6dc6b7721
dblancoformacion <dblancoformacion@gmail.com>
2019-12-13 18:27:58 +0100

    Enlace a renovar palabra

--- ahorcado.php
+++ ahorcado.php
@@ -1,3 +1,5 @@
+<div style="margin:10px"><a href="ahorcado.php">Otra palabra</a></div>
+
 <?php
 
 if(isset($_POST['resultado'])){
@@ -35,7 +37,7 @@ function formulario_resolver($p){
 function formulario_probar($p){
 	$r=null;
 	$r.='<form method="post">';
-	$r.='<input name="letra" size="1">';
+	$r.='<input name="letra" size="1" autofocus>';
 	if(isset($_POST['letras'])) for($i=0;$i<iconv_strlen($p);$i++){
 		if(!isset($_POST['letras'][$i])) $_POST['letras'][$i]='';
 		$r.='<input type="hidden" name="letras[]" value="'.$_POST['letras'][$i].'">';
